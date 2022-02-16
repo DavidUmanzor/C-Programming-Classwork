82511c03e13f4ebd570c98140fdd5554674ae4e0
Zarthanix <53411486+Zarthanix@users.noreply.github.com>
2022-02-16 00:12:35 -0500

    Update calories.c

--- calories.c
+++ calories.c
@@ -12,30 +12,53 @@ int main(void){
     int FOOD_CAL = 40;
     int CALS_PER_POUND = 3500;
 
-    int Minutes_Stand, Minutes_Walk, Minutes_Drink, Minutes_Eat;
-
-    printf("How many minutes were you standing today?\n");
-    scanf("%d", &Minutes_Stand);
+    int Minutes_Walk, Minutes_Stand, Minutes_Drink, Minutes_Eat;
 
     printf("How many minutes were you walking today?\n");
     scanf("%d", &Minutes_Walk);
 
-    printf("How many minutes were you eating today?\n");
-    scanf("%d", &Minutes_Eat);
+    printf("How many minutes were you standing today?\n");
+    scanf("%d", &Minutes_Stand);
 
     printf("How many minutes were you drinking today?\n");
     scanf("%d", &Minutes_Drink);
 
+    printf("How many minutes were you eating today?\n");
+    scanf("%d", &Minutes_Eat);
+
     int Calories_Burned = (Minutes_Stand * STAND_CAL) + (Minutes_Walk * WALK_CAL);
     int Calories_Gained = (Minutes_Drink * DRINK_CAL) + (Minutes_Eat * FOOD_CAL);
 
-    int Calories_Total = Calories_Burned - Calories_Gained;
-    int Pounds_Total = Calories_Total / CALS_PER_POUND;
 
-    If (Calories_Total0) {
-        printf("You lost %d pounds today!", Pounds_Total);
+    //Comments here were to help check for calculation errors
+
+    //printf("Calories from Standing (%d)\n", Minutes_Stand * STAND_CAL);
+    //printf("Calories from Walking (%d)\n", Minutes_Walk * WALK_CAL);
+
+    //printf("Total Calories Burned (%d)\n", Calories_Burned);
+
+    //printf("Calories from Drinking (%d)\n", Minutes_Drink * DRINK_CAL);
+    //printf("Calories from Eating (%d)\n", Minutes_Eat * FOOD_CAL);
+
+    //printf("Total Calories Gained (%d)\n", Calories_Gained);
+
+    int Calories_Total = 0;
+
+    Calories_Total = Calories_Burned - Calories_Gained;
+
+    //printf("%d\n", Calories_Total);
+
+    double Pounds_Total = 0;
+
+    Pounds_Total = (double)Calories_Total / (double)CALS_PER_POUND;
+
+
+    //printf("%d\n", Pounds_Total);     
+
+    if (Calories_Total >= 0) {
+        printf("You lost %.3lf pounds today!", Pounds_Total);
     }
-    Else {
-        printf("Weight lost is -%d pounds.", Pounds_Total);
+    else {
+        printf("Weight lost is %.3lf pounds.", Pounds_Total);
     }
 }
